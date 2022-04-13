7b83404789966a2fb79e93415222c00b950eb147
Jarodcox <60438062+Jarodcox@users.noreply.github.com>
2022-04-13 16:12:25 -0500

    First

---
+++ CoxApp.py
@@ -0,0 +1,13 @@
+import streamlit as st
+import numpy as np
+
+st.title("Test")
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+st.write('Count = ', st.session_state.count)
+
